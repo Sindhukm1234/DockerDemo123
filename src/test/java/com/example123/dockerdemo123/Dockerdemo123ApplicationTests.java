7f04c66ae53748ed2cccfb22fc52b57b2de022c7
Sindhukm1234 <92727444+Sindhukm1234@users.noreply.github.com>
2021-10-21 12:58:28 +0530

    Add files via upload

---
+++ src/test/java/com/example123/dockerdemo123/Dockerdemo123ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example123.dockerdemo123;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dockerdemo123ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
